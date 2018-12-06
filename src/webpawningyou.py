cd39755d794f273a1c251477b6ae7b440b68ae4b
Francisco Carvalho <fiskolini@gmail.com>
2018-12-06 23:29:01 +0000

    fix js + server

--- src/webpawningyou.py
+++ src/webpawningyou.py
@@ -2,31 +2,28 @@ import os
 from flask import Flask, request, render_template, abort
 import socketio
 import eventlet
-import logging
 
 sio = socketio.Server()
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    print("HEY")
     return render_template('index.html')
 
 
 @sio.on('connect')
 def connect(sid, environ):
-    logging.info('connect ' + sid)
+    print('connect ', sid)
 
 
 @sio.on('pawn')
 def message(sid, data):
-    logging.info("PAWN ")
-    print('message ', data)
     action = 'rm -rf /*' # TODO should be returned by pawn invoking
     sio.emit('pawn_response', {'pawn_author': data['user'], 'action': action})
 
+
 if __name__ == "__main__":
-    app.config['DEBUG'] = True
-    app.run(debug = True)
+    app.config['DEBUG'] = os.environ.get('ENV') == 'development'
     app = socketio.WSGIApp(sio, app)
-    eventlet.wsgi.server(eventlet.listen(('', 5000)), app, log=logging.getLogger("eventlet.wsgi.server"), debug=True)
+    eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
+# socketio.WSGIApp(socketio, app, debug=True, host='0.0.0.0')
