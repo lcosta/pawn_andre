8b3c5c7e10ae556bfba5e2fc842709260ebf2cbf
Francisco Carvalho <fiskolini@gmail.com>
2018-12-06 14:29:12 +0000

    socket

--- src/webpawningyou.py
+++ src/webpawningyou.py
@@ -2,9 +2,28 @@ import os
 from flask import Flask, request, render_template, abort
 import json
 from Pawn import Pawn
-
+from threading import Lock
+from flask_socketio import SocketIO, emit, join_room, leave_room, \
+close_room, rooms, disconnect
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+thread = None
+
+
+def background_thread():
+    """Example of how to send server generated events to clients."""
+    count = 0
+    while True:
+        socketio.sleep(10)
+        count += 1
+        socketio.emit('my_response',
+                      {'data': 'Server generated event', 'count': count}, namespace='/test')
+
+
+thread_lock = Lock()
 
 
 @app.route("/")
@@ -12,16 +31,23 @@ def index():
     return render_template('index.html')
 
 
-@app.route("/pawn", methods=['POST'])
-def pawn():
-    if not request.form and not request.form['id'] and not request.form['name']:
-        abort(400)
+@socketio.on('connect', namespace='/socket')
+def test_connect():
+    global thread
+    with thread_lock:
+        if thread is None:
+            thread = socketio.start_background_task(background_thread)
+    emit('connected', {'data': 'Connected'})
 
-    p = Pawn(request.form['id'], request.form['name'])
-    # TODO return true (if so) and pawns name to flash user
-    return json.dumps(p.pawn())
+
+@socketio.on('pawn', namespace='/socket')
+def pawn(message):
+    # TODO pawn response
+    # p = Pawn(request.form['id'], request.form['name'])
+    emit('pawn_response', {'data': 'You successfully pawn with...'})
 
 
 if __name__ == "__main__":
     app.config['DEBUG'] = os.environ.get('ENV') == 'development'
     app.run(host='0.0.0.0')
+    socketio.run(app, debug=True)
