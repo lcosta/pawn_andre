4c671e07d5246f1a0bb9de3c9606c0ebf29b8f7b
Johny Serpa <joao.serpapinto@gmail.com>
2018-12-07 14:50:49 +0000

    tau

--- src/scripts/spam_notification.sh
+++ src/scripts/spam_notification.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
-
-notify-send -i face-wink "Never trust a computer you can’t throw out a window."
+phrase[0]="You're not a complete idiot. Some pieces are missing."
+phrase[1]="One man’s crappy software is another man’s full time job."
+phrase[2]="System Shutdown"
+phrase[3]="Deleting root"
+phrase[4]="You've been pawned!"
+phrase[5]="Never trust a computer you can’t throw out a window."
+phrase[6]="You're not a complete idiot. Some pieces are missing."
+phrase[7]="Have you ever wondered why you can’t taste your tongue?"
+phrase[8]="I never apologise. I’m sorry, that’s just the way I am."
+phrase[9]="You laugh because imm different,i laugh because I just farted.."
+phraser=${phrase[$RANDOM % ${#phrase[@]} ]}
+notify-send -i face-wink "$phraser"
