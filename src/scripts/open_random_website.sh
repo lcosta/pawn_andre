2a32c67d2c9faf46fe820387416c555b7e9ad202
Johny Serpa <joao.serpapinto@gmail.com>
2018-12-07 14:54:29 +0000

    more sites

--- src/scripts/open_random_website.sh
+++ src/scripts/open_random_website.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
-
-sensible-browser "http://www.nyan.cat/"
+expressions[0]="http://www.nyan.cat/"
+expressions[1]="https://geekprank.com/hacker/"
+expressions[2]="https://answers.yahoo.com/question/index;_ylt=AwrC1zF2fwpcpBgA3yxPmolQ;_ylu=X3oDMTEybTFvb2wxBGNvbG8DYmYxBHBvcwMxBHZ0aWQDQjI1NTlfMQRzZWMDc3I-?qid=20081210174759AADf9SE"
+expressions[3]="https://answers.yahoo.com/question/index;_ylt=AwrC1Cqxfwpcol4AkipPmolQ;_ylu=X3oDMTEybTFvb2wxBGNvbG8DYmYxBHBvcwMxBHZ0aWQDQjI1NTlfMQRzZWMDc3I-?qid=20110126015747AAlxX4M"
+selectedexpression=${expressions[$RANDOM % ${#expressions[@]} ]}
+sensible-browser "$selectedexpression"
